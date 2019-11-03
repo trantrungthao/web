37e91768c7c6fbd03648013a6c086dbb0c26c61d
trantrungthao <55526199+trantrungthao@users.noreply.github.com>
2019-11-03 21:17:35 +0700

    webnew

---
+++ js/tab.js
@@ -0,0 +1,21 @@
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+   tablinks[0].className = tablinks[0].className.replace(" active", "");
+  for (i = 1; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
